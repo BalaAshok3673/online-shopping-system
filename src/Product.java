c1f2c213c9f4f7868cb47623196a8b351113ce22
bala kumar <balaashok2004@gmail.com>
2025-10-07 18:07:58 +0530

    Added Product.java class and implemented get/set methods

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,56 @@
-abstract  class Product {
+public   class Product {
     private int id;
     private int quantity;
     private String name;
     private double price;
+    private static int count;
 
-    public product
+    public Product(int id , int quantity , String name , double price){
+            this.name=name;
+            this.quantity=quantity;
+            this.price=price;
+            this.id=++count;
+    }
+
+    public String getName(){
+        return  name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+    public void showDetails(){
+        System.out.println("ID: " + id + " | Name: " + name +
+                " | Quantity: " + quantity +
+                " | Price: ₹" + price);
+    }
+
+    public void reduceQuantity(int amount){
+            if (amount>0 && amount<=quantity){
+                quantity -= amount;
+            }else {
+                System.out.println("out of stock");
+            }
+    }
 }
